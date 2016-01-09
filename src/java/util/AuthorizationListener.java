4abb9e21821f6533045c3643d83ea025bcbbbb62
T3r1jj <T3r1jj@gmail.com>
2016-01-09 14:24:57 +0100

    Added license to java files

--- src/java/util/AuthorizationListener.java
+++ src/java/util/AuthorizationListener.java
@@ -1,3 +1,18 @@
+/* 
+ * Copyright 2016 Damian Terlecki.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package util;
 
 import javax.faces.application.NavigationHandler;
@@ -8,10 +23,6 @@ import javax.faces.event.PhaseListener;
 import javax.servlet.http.HttpSession;
 import jpa.entity.Person;
 
-/**
- *
- * @author Damian Terlecki
- */
 public class AuthorizationListener implements PhaseListener {
 
     @Override
